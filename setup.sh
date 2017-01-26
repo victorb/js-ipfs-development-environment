a5eb1447a2328539083ebd085b3f43b9c585255e
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-01-26 14:21:49 +0100

    Add setup step

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+echo "## Initializing submodules"
+git submodule init
+
+echo "## Updating submodules"
+git submodule update
+
+echo "## Installing lerna"
+npm install
+
+echo "## Bootstrap modules"
+./node_modules/.bin/lerna bootstrap
+
+echo "## Running tests"
+./node_modules/.bin/lerna run test
